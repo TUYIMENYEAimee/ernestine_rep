3507193811197cb60cbf78b3166db035e3eee463
TUYIMENYE <tuyimenyeaimee@gmail.com>
2024-03-21 12:29:10 +0200

    Docarization is completed

---
+++ SRC/login page.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Code</title>
+  </head>
+  <body><center>
+    <a href="registration.php">LOGIN Form</a>
+    <a href="login.php">Login Form</a><hr>
+    <h1>REgistration</h1>
+    <form action="" methode="POST">
+  <label for="emalil">email:</label><br>
+  <input type="email" id="email" name="email"><br>
+  <label for="password">Password:</label><br>
+  <input type="password" id="password" name="password"><br>
+  <button name="btn">LOGIN</button>
+</form>
+  </body>
+</html>
